0c7eed1f7011a1a34a8d6d7756a970333888d725
mput <mputilovskij@gmail.com>
2024-03-18 18:20:28 +0500

    добавлена функция "Добавление работы"

--- api/jobs_api.py
+++ api/jobs_api.py
@@ -2,7 +2,7 @@ import flask
 from data import db_session
 
 from data.jobs import Jobs
-from flask import jsonify, make_response
+from flask import jsonify, make_response, request
 
 blueprint = flask.Blueprint(
     'jobs_api',
@@ -31,3 +31,23 @@ def get_one_job(job_id):
             'jobs': jobs.to_dict()
         }
     )
+
+
+@blueprint.route('/api/jobs', methods=['POST'])
+def create_job():
+    if not request.json:
+        return make_response(jsonify({'error': 'Empty request'}), 400)
+    elif not all(key in request.json for key in
+                 ['team_leader', 'job', 'work_size', 'collaborators', 'is_finished']):
+        return make_response(jsonify({'error': 'Bad request'}), 400)
+    db_sess = db_session.create_session()
+    job = Jobs(
+        team_leader=request.json['team_leader'],
+        job=request.json['job'],
+        work_size=request.json['work_size'],
+        collaborators=request.json['collaborators'],
+        is_finished=request.json['is_finished']
+    )
+    db_sess.add(job)
+    db_sess.commit()
+    return jsonify({'id': job.id})
